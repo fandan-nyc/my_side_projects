12485c39f1982bc9af63c6cb47f16fb2afb8ce3d
Dan Fan <4644927+fandan-nyc@users.noreply.github.com>
2021-07-24 08:33:24 -0500

    Update vector_template.cpp

--- notes/cpp_notes/chapter_3/vector_template.cpp
+++ notes/cpp_notes/chapter_3/vector_template.cpp
@@ -21,15 +21,36 @@ class Vector {
         T& operator[](int i){
             return ele[i];
         }
+        
+        const int length() const {
+            return size;
+        }
+        // to leverage the for(auto& x: vectorList){}
+        // we need to define begin and end;
 };
 
+template<typename T> 
+T* begin(Vector<T>& vector){
+    return vector.length() == 0 ? nullptr : &vector[0];
+}
+
+template<typename T> 
+T* end(Vector<T>& vector){
+    return begin(vector) + vector.length();
+}
+
+
 int main(){
-    Vector<double> doubleVector {4};
+    Vector<double> doubleVector = Vector<double>(4);
     doubleVector[2] = 10.2;
     cout << doubleVector[2] << endl;
     
-    Vector<int> intVector {10};
+    Vector<int> intVector = Vector<int>(4);
     intVector[1] = 10.22;
     cout << intVector[1] << endl;
+    
+    for(auto& v: doubleVector){
+        cout << v << endl;
+    } // here we override begin and end in the std::begin / std::end
     
 }
